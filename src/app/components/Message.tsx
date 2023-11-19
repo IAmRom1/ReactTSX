910a54828df4b0122b72207775fc7d0cf128072d
Romain <git@romainsavigny.be>
2023-11-19 17:48:12 +0100

    1.0

---
+++ src/app/components/Message.tsx
@@ -0,0 +1,26 @@
+import { MessagesInt } from "../model";
+
+type Props = {
+    mess: MessagesInt;
+    messData: MessagesInt[];
+    setMessData:React.Dispatch<React.SetStateAction<MessagesInt[]>>
+}
+
+const Message = ({mess, messData, setMessData}: Props) => {
+    const dateFormter = (date:number)=>{
+        return new Date(date).toLocaleDateString('fr-BE', {day:"numeric",
+        month:'long'})
+    }
+    const handleDelete = ()=>{
+        setMessData(messData.filter((el)=>el.id !== mess.id))
+    }
+    return (
+        <div className="message-container">
+            <p>{mess.message}</p>
+            <h5>{dateFormter(mess.date)}</h5>
+            <span id="delete" onClick={() => handleDelete()}>&#10008;</span>
+        </div>
+    );
+};
+
+export default Message;
